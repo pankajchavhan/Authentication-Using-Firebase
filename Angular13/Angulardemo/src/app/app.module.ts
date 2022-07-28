77e49d97dd0cbb458b8c0aae62977e9d13c4de5c
pankaj chavhan <pankajchavhan5@gmail.com>
2022-07-28 15:38:12 +0530

    login component

--- Angular13/Angulardemo/src/app/app.module.ts
+++ Angular13/Angulardemo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
